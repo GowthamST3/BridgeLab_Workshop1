6c5d7fcc063d4f1f266ada1b7463b54d5e01ebd7
GowthamST3 <gowthamgiri3@gmail.com>
2022-04-20 09:22:43 +0530

    Added- GamblerGameUC1 code - Initialized StakePreday and BetPerGame

---
+++ GamblerGameUC1.java
@@ -0,0 +1,12 @@
+package com.BridgeLabz_GamblerGame;
+
+public class GamblerGameUC1 {
+	   static final int STAKEPERDAY=100;
+       static final int BETPERGAME=1;
+       
+          public static void main(String[] args) {
+		    System.out.println("Stake per day is "+ STAKEPERDAY );
+	        System.out.println("Bet per Game is "+ BETPERGAME );
+	        
+	}
+}
